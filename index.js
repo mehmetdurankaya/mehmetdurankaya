356af43a802d9e90f0af837837065b365659f971
Mehmet Duran Kaya <44356848+mehmetdurankaya@users.noreply.github.com>
2024-01-26 19:33:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,163 @@
+const thisYear = new Date().getFullYear()
+const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
+const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
+const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
+const progressBarOfThisYear = generateProgressBar()
 
+let monthNames = ["Jan","Feb","Mar","Apr", "May","Jun","Jul","Aug", "Sep", "Oct","Nov","Dec"];
+
+function generateProgressBar() {
+    const progressBarCapacity = 30
+    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
+    const progressBar = Array(progressBarCapacity)
+        .fill('▁')
+        .map((value, index) => index < passedProgressBarIndex ? '█' : value)
+        .join('')
+    return `{ ${progressBar} }`
+}
+
+const readme = `\
+   <img align="right" src="https://readme-typing-svg.herokuapp.com/?lines=+Full-stack+Dev+|+Mehmet+Duran+Kaya;&center=true&vCenter=true&pause=200&width=800&height=20" width="100%" /> 
+ 
+# Hi there! <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/Hi.gif" width="35" />
+### <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/Developer.gif" width="45" /> About Me:
+  <img align="right" alt="GIF" src="https://media.giphy.com/media/dWesBcTLavkZuG35MI/giphy.gif" width="450" height="250"/>
+  
+- 🏦  I'm Full-Stack Developer <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30">
+- 🔭 I’m currently working on ...   I'm writing the algorithm's right now
+- 🌱 I’m currently learning ...     Java-Vuejs-Electronjs-istatistic
+- ⚡ I’m an instructor      ...     Kickboks - Karate -Close Defence  
+- &#129354;  I'm happy when I do ... Combat sports and Coding
+
+
+
+
+### Connect with me:
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-%230077B5.svg?logo=linkedin&logoColor=white)](https://www.linkedin.com/in/mehmetdurankaya)
+[<img align="left" height="24" width="24" src="https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/gmail.svg" />][gmail]
+[![Github Badge](https://img.shields.io/badge/-Github-000?style=quare&labelColor=000&logo=Github&logoColor=white&link=link)](https://github.com/mehmetdurankaya)
+[![Instagram Badge](https://img.shields.io/badge/-Instagram-C13584?style=flat-quare&labelColor=C13584&logo=instagram&logoColor=white&link=link)](https://www.instagram.com/kayamehmetduran)
+
+[linkedin]: https://www.linkedin.com/in/mehmetdurankaya/
+[gmail]: mailto:mehmetdurankaya@gmail.com
+[hackerrank]: https://www.hackerrank.com/mehmetdurankaya
+
+### languages ​​that interest me!
+
+
+<p align="left">
+      <img src="https://www.vectorlogo.zone/logos/java/java-icon.svg" alt="java" width="65" height="65"/>
+      <img src="https://www.vectorlogo.zone/logos/springio/springio-icon.svg" alt="spring" width="65" height="65"/> 
+      <img src="https://www.vectorlogo.zone/logos/hibernate/hibernate-icon.svg" alt="Hibernate" width="55" height="55"/>
+      <img src="https://www.vectorlogo.zone/logos/python/python-icon.svg" alt="python" width="65" height="65"/>
+      <img src="https://www.vectorlogo.zone/logos/golang/golang-icon.svg"alt="GoLang" width="65" height="65" />
+      <img src="https://www.vectorlogo.zone/logos/nodejs/nodejs-icon.svg" alt="Nodejs" width="65" height="65"/>    
+      <img src="https://www.vectorlogo.zone/logos/mysql/mysql-icon.svg" alt="mysql" width="65" height="65"/>
+      <img src="https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg" alt="mongodb" width="65" height="65"/>
+      <img src="https://www.vectorlogo.zone/logos/docker/docker-official.svg" alt="docker" width="65" height="65"/>     
+      <img src="https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg" alt="Html5" width="65" height="65"/>
+      <img src="https://www.vectorlogo.zone/logos/github/github-icon.svg" alt="Github" width="65" height="65"/>
+      <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="Postman" width="65" height="65"/>
+      <img src="https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg" alt="postgresql" width="65" height="65"/>
+      <img src=" https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg" alt="W3CSS" width="65" height="65"/>
+      <img src="https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg" alt="" width="65" height="65"/>
+
+  
+ 
+</p>
+
+---
+### 📺 My Youtube Videos
+<!-- YOUTUBE:START -->
+- [Kartal'ın yeniden doğuşu](https://www.youtube.com/watch?v=8G6w-x-WqAM)
+- [Konsantrasyon ve Kararlılık](https://www.youtube.com/watch?v=t_p6djMEzCA)
+- [🛑 Stop No pain no gain](https://www.youtube.com/watch?v=Rk3ZinyVwzk)
+- [Nunchaku](https://www.youtube.com/watch?v=r4Hn9QDmzgk)
+- [Keep Your Packages Up-To-Date with this VS Code Extension]()
+  <!-- YOUTUBE:END -->   
+
+### <img alt="GIF" src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/hmm.gif" width="20" /> A Famous Fact/Quote:
+<a href="https://github.com/marketplace/actions/quote-readme">
+<!--STARTS_HERE_QUOTE_README-->
+<i>❝IMDb is one of the oldest websites on the internet, and began on Usenet in 1990 as a list of “actresses with beautiful eyes.”❞</i>
+<!--ENDS_HERE_QUOTE_README-->
+</a>
+
+---
+   <img align="right" src="https://readme-typing-svg.herokuapp.com/?lines=+Full-stack+Dev+|+Mehmet+Duran+Kaya;&center=true&vCenter=true&pause=200&width=800&height=20" width="100%" /> 
+
+---
+
+### <img src='https://media1.giphy.com/media/du3J3cXyzhj75IOgvA/giphy.gif?cid=ecf05e47x2g034i9pzwtzzsd3xgg2w9nr94t4tflbbgo3008&rid=giphy.gif' width='25' /> My Github Stats:
+
+
+![Apoorv's github stats](https://github-readme-stats.vercel.app/api?username=mehmetdurankaya&show_icons=true&title_color=ffc857&icon_color=8ac926&text_color=daf7dc&bg_color=151515&hide=issues&count_private=true&include_all_commits=true)
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=mehmetdurankaya&layout=compact&text_color=daf7dc&bg_color=151515&hide=css,html,php)](https://github.com/anuraghazra/github-readme-stats)
+[![GitHub Streak](https://github-readme-streak-stats.herokuapp.com/?user=mehmetdurankaya&theme=dark)](https://git.io/streak-stats)
+
+<!--START_SECTION:waka-->
+
+<!--END_SECTION:waka-->
+
+⏳ **Year Progress** ${progressBarOfThisYear} ${(
+  progressOfThisYear * 100
+).toFixed(2)} % as on ⏰ ${
+  new Date().getDate() -
+  1 +
+  "-" +
+  monthNames[new Date().getMonth()] +
+  "-" +
+  new Date().getFullYear()
+}
+
+---
+
+### 📺 My Youtube Videos
+<!-- YOUTUBE:START -->
+- [Kartal'ın yeniden doğuşu](https://www.youtube.com/watch?v=8G6w-x-WqAM)
+- [Konsantrasyon ve Kararlılık](https://www.youtube.com/watch?v=t_p6djMEzCA)
+- [🛑 Stop No pain no gain](https://www.youtube.com/watch?v=Rk3ZinyVwzk)
+- [Nunchaku](https://www.youtube.com/watch?v=r4Hn9QDmzgk)
+- [Keep Your Packages Up-To-Date with this VS Code Extension]()
+  <!-- YOUTUBE:END -->   
+
+---
+### <img alt="GIF" src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/hmm.gif" width="20" /> A Famous Fact/Quote:
+<a href="https://github.com/marketplace/actions/quote-readme">
+<!--STARTS_HERE_QUOTE_README-->
+<i>❝IMDb is one of the oldest websites on the internet, and began on Usenet in 1990 as a list of “actresses with beautiful eyes.”❞</i>
+<!--ENDS_HERE_QUOTE_README-->
+</a>
+---
+   <img align="right" src="https://readme-typing-svg.herokuapp.com/?lines=+Full-stack+Dev+|+Mehmet+Duran+Kaya;&center=true&vCenter=true&pause=200&width=800&height=20" width="100%" /> 
+<br />
+---
+<div align="center">
+  <img src="https://github-readme-stats.vercel.app/api?username=mehmetdurankaya&show_icons=true&title_color=ffc857&icon_color=8ac926&text_color=daf7dc&bg_color=151515&hide=issues&count_private=true&include_all_commits=true" alt="Apoorv's github stats" />
+  <a href="https://github.com/anuraghazra/github-readme-stats">
+    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=mehmetdurankaya&layout=compact&text_color=daf7dc&bg_color=151515&hide=css,html,php" alt="Top Languages" />
+  </a>
+</div>
+<div align="center">
+[![GitHub Streak](https://github-readme-streak-stats.herokuapp.com/?user=mehmetdurankaya&theme=dark)](https://git.io/streak-stats)
+</div>
+---
+<br />
+
+<div align="center">
+  <img src="https://profile-counter.glitch.me/mehmetdurankaya/count.svg?"  />
+</div>
+---
+### <img alt="GIF" src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/hmm.gif" width="20" /> A Famous Fact/Quote:
+<a href="https://github.com/marketplace/actions/quote-readme">
+<!--STARTS_HERE_QUOTE_README-->
+<i>❝IMDb is one of the oldest websites on the internet, and began on Usenet in 1990 as a list of “actresses with beautiful eyes.”❞</i>
+<!--ENDS_HERE_QUOTE_README-->
+</a>
+
+---
+
+### <img align ='center' src='https://media2.giphy.com/media/UQDSBzfyiBKvgFcSTw/giphy.gif?cid=ecf05e47p3cd513axbek3f56ti3jzizq8hincw20jauyyfyw&rid=giphy.gif' width ='29' /> Here's some humor for you:
+<img src="https://readme-jokes.vercel.app/api" alt="Error fetching resource, Refresh again to view Jokes Card" width = '11000' />
+`;
+console.log(readme)
